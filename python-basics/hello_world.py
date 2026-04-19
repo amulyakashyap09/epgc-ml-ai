848cc3406509a8efd547d8bc6341b9ee679b3e58
Amulya Kashyap <amulyak@adobe.com>
2026-04-20 02:41:04 +0530

    learning day 1

---
+++ python-basics/hello_world.py
@@ -0,0 +1,19 @@
+# defining var
+
+my_var = 1
+my_next_var = 2
+my_first_variable = "Now, I'm a string."
+
+print(my_var)
+print(my_next_var)
+print(my_first_variable)
+
+# defining function in python
+def hello():
+    return "Hello, World!"
+
+def add_two_num(n1, n2):
+    return n1+n2
+
+print(hello())
+print(add_two_num(1, 2))
